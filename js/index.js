53ad22c41c50c7426298fe347d85c7e6da5d911b
Iconmaster <crazybonesj@yahoo.com>
2015-08-23 18:40:10 -0400

    Testing out even more stuff

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function load_content() {
 	$("#main-content").load("html/test.html");
 }
+
+function load_content_2() {
+	$.get("php/test.php", function(data, status) {
+		$("#main-content").html(data);
+	});
+}
---
+++ php/test.php
@@ -0,0 +1,3 @@
+<?php
+echo "Aaaaand THIS is me testing to see if github.io will let me use server-side pages (in this case, PHP).";
+?>
