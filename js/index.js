8233a7f2109b86ca7ad1d124bcf19ad7ea615628
Iconmaster <crazybonesj@yahoo.com>
2015-08-24 10:24:33 -0400

    Added code for common header/footer stuff

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
 function load_content() {
 	$("#main-content").load("html/test.html");
 }
-
-function load_content_2() {
-	$.get("php/test.php", function(data, status) {
-		$("#main-content").html(data);
-	});
-}
--- php/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Aaaaand THIS is me testing to see if github.io will let me use server-side pages (in this case, PHP).";
-?>
