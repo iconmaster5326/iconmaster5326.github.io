91ab561a336df870ad2899524a7606f0e8a49f3b
Iconmaster <crazybonesj@yahoo.com>
2015-08-24 16:57:31 -0400

    Made the transitions PRETTY

--- js/common.js
+++ js/common.js
@@ -1,4 +1,15 @@
-$("#header").load("/html/header.html", function() {
-	$("#"+active_menu_item).addClass("active");
-});
+$("#header").load("/html/header.html");
 $("#footer").load("/html/footer.html");
+$("#main-content").load("/html/index.html");
+
+iconus = {};
+iconus.loadPage = function(id, page) {
+	$(".nav-pills .active").removeClass("active");
+	$("#menu-item-"+id).addClass("active");
+	$("#main-content").fadeOut(100,function() {
+		$("#main-content").load(page);
+		$("#main-content").fadeIn(100);
+	});
+	
+	
+};
